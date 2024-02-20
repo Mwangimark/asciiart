07ab42afe7e512056ff04638af1dd5bb004172f2
Mwangimark <132432689+Mwangimark@users.noreply.github.com>
2024-02-20 18:20:24 +0000

    run with python

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# Form implementation generated from reading ui file 'main.ui'
+
 print('''
        #     #    #    ######  #    #          #####  #######  #####  #     #    #    ######   ##### 
        ##   ##   # #   #     # #   #          #     # #     # #     # ##   ##   # #   #     # #     #
